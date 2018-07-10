b44da40c1c099615356d2b47ad9a26446f7dee4e
CHBaker <charlesprobaker@mail.com>
2018-07-09 19:45:50 -0600

    FEATURE: increment/decrement methods
    - Methods and state for adjusting the form values for the day

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View>
-        {getMetricMetaInfo('bike').getIcon()}
+        { getMetricMetaInfo('bike').getIcon() }
       </View>
     );
   }
--- components/AddEntry.js
+++ components/AddEntry.js
@@ -3,6 +3,45 @@ import { View, Text } from 'react-native';
 import { getMetricMeetaInfo } from '../utils/helper';
 
 export default class AddEntry extends Component {
+    
+    state = {
+        run: 0,
+        bike: 0,
+        swim: 0,
+        sleep: 0,
+        eat: 0,
+    }
+    
+    increment = (metric) => {
+        const { max, step } = getMetricMeetaInfo(metric);
+
+        this.setState((state) => {
+            const count = state[metric] + step;
+
+            return {
+                ...state,
+                [metric]: count > max ? max : count
+            };
+        })
+    }
+
+    decrement = (metric) => {
+        this.setState((state) => {
+            const count = state[metric] - getMetricMeetaInfo(metric).step;
+
+            return {
+                ...state,
+                [metric]: count > 0 ? 0 : count
+            };
+        })
+    }
+
+    slide = (metric, value) => {
+        this.setState(() => ({
+            [metric]: value
+        }));
+    }
+
     render() {
         return (
             <View>
