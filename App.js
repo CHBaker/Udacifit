567b56d4d95547d5415eac9753733caf3f5cf30a
CHBaker <charlesprobaker@mail.com>
2018-07-14 16:35:48 -0600

    FEATUREE: add live component
    - Will host native interactions

--- App.js
+++ App.js
@@ -10,13 +10,14 @@ import { purple, white } from './utils/colors';
 import { FontAwesome, Ionicons } from '@expo/vector-icons';
 import { Constants } from 'expo';
 import EntryDetail from './components/EntryDetail';
+import Live from './components/Live';
 
 function UdaciStatusBar ({ backgroundColor, ...props }) {
   return (
     <View
       style={{ backgroundColor, height: Constants.statusBarHeight }}
     >
-      <StatusBar translucent backgroundColo={ backgroundColor } {...props} />
+      <StatusBar translucent backgroundColor={ backgroundColor } {...props} />
     </View>
   )
 }
@@ -34,6 +35,13 @@ const Tabs = TabNavigator({
     navigationOptions: {
       tabBarLabel: 'Add Entry',
       tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={ 30 } color={ tintColor} />
+    }
+  },
+  Live: {
+    screen: Live,
+    navigationOptions: {
+      tabBarLabel: 'Live',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-speedometer' size={ 30 } color={ tintColor }/>
     }
   }
 }, {
