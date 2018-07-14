aaafef39d03838fcdece231bf561678b3ede8730
CHBaker <charlesprobaker@mail.com>
2018-07-14 12:17:01 -0600

    FEATURE: loading component to reduce jank

--- components/History.js
+++ components/History.js
@@ -8,8 +8,13 @@ import UdacifitnessCalendar from 'udacifitness-calendar';
 import { white } from '../utils/colors';
 import DateHeader from './DateHeader';
 import MetricCard from './MetricCard';
+import { AppLoading } from 'expo';
 
 class History extends Component {
+
+    state = {
+        ready: false
+    }
     
     componentDidMount() {
         const { dispatch } = this.props;
@@ -23,6 +28,7 @@ class History extends Component {
                     }))
                 }
             })
+            .then(() => this.setState({ ready: true }))
             .catch((error) => console.log(error));
     }
 
@@ -58,6 +64,11 @@ class History extends Component {
     
     render() {
         const { entries } = this.props;
+        const { ready } = this.state;
+
+        if (ready === false) {
+            return <AppLoading />
+        };
 
         return (
             <UdacifitnessCalendar
